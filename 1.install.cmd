cf0d0c1b61cdcf812e880827a09b06344751a17b
CNMan <nklijl@mail.nankai.edu.cn>
2022-03-22 17:06:24 +0800

    Tue 22 Mar 2022 05:06:24 PM CST

--- 1.install.cmd
+++ 1.install.cmd
@@ -1,5 +1,5 @@
 @echo off
-wget --append-output=installlog.txt --no-verbose -N --no-if-modified-since --header "Host: update.geo.drweb.com" --header "User-Agent: DrWebUpdate-12.0.50.02100 (windows: 5.1.2600)" --header "Accept: */*" --header "X-DrWeb-KeyNumber: 0148784619" --header "X-DrWeb-Validate: 83b9f510627c05c0afcb5be9e1b46559" --header "X-DrWeb-SysHash: C7B5A8334AF24F3CF842371701E76DD1" --header "Cache-Control: no-cache" --header "Pragma: no-cache" --header "Connection: keep-alive" --no-cookies -P baserepo -i dl1250
+wget --append-output=installlog.txt --no-verbose -N --no-if-modified-since --header "Host: update.geo.drweb.com" --header "User-Agent: DrWebUpdate-12.0.50.02100 (windows: 5.1.2600)" --header "Accept: */*" --header "X-DrWeb-KeyNumber: 0149095031" --header "X-DrWeb-Validate: e8a8c5c9370ce8e60f1f3ba4d6fc6962" --header "X-DrWeb-SysHash: C7B5A8334AF24F3CF842371701E76DD1" --header "Cache-Control: no-cache" --header "Pragma: no-cache" --header "Connection: keep-alive" --no-cookies -P baserepo -i dl1250
 7z x baserepo\*.lzma
 sc create DrWebAVService binpath= "e:\GitHub\drweb\dwscancl\dwservice.exe" start= demand
 sc create DrWebEngine binpath= "e:\GitHub\drweb\dwscancl\dwengine.exe" start= demand
--- 4.update.cmd
+++ 4.update.cmd
@@ -11,7 +11,7 @@ TASKKILL /IM dwservice.exe /F
 TASKKILL /IM dwsewsc.exe /F
 TASKKILL /IM dwwatcher.exe /F
 ping -n 60 127.0.0.1 > nul
-wget --append-output=updatelog.txt --no-verbose -N --no-if-modified-since --header "Host: update.geo.drweb.com" --header "User-Agent: DrWebUpdate-12.0.50.02100 (windows: 5.1.2600)" --header "Accept: */*" --header "X-DrWeb-KeyNumber: 0148784619" --header "X-DrWeb-Validate: 83b9f510627c05c0afcb5be9e1b46559" --header "X-DrWeb-SysHash: C7B5A8334AF24F3CF842371701E76DD1" --header "Cache-Control: no-cache" --header "Pragma: no-cache" --header "Connection: keep-alive" --no-cookies -P baserepo -i dl1250
+wget --append-output=updatelog.txt --no-verbose -N --no-if-modified-since --header "Host: update.geo.drweb.com" --header "User-Agent: DrWebUpdate-12.0.50.02100 (windows: 5.1.2600)" --header "Accept: */*" --header "X-DrWeb-KeyNumber: 0149095031" --header "X-DrWeb-Validate: e8a8c5c9370ce8e60f1f3ba4d6fc6962" --header "X-DrWeb-SysHash: C7B5A8334AF24F3CF842371701E76DD1" --header "Cache-Control: no-cache" --header "Pragma: no-cache" --header "Connection: keep-alive" --no-cookies -P baserepo -i dl1250
 7z x baserepo\*.lzma -y
 echo virus database update has been completed
 pause
